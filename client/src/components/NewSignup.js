181575af00f583646ed0826da3d2704cec7c96f9
betalantz <betalantz@gmail.com>
2021-09-02 15:27:59 -0700

    coerce activityId to int before sending in req body

--- client/src/components/NewSignup.js
+++ client/src/components/NewSignup.js
@@ -16,7 +16,7 @@ function NewSignup({ camperId, onAddActivity }) {
     e.preventDefault();
     const formData = {
       camper_id: camperId,
-      activity_id: activityId,
+      activity_id: Number(activityId),
       time: Number(time),
     };
     fetch("/signups", {

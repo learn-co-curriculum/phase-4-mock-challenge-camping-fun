81a3e277334ed9ebcdb47fe75dc30c84fac65555
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-06 14:22:27 +0300

    created activity resource

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,23 @@
+class ActivitiesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+
+def index 
+    activity = Activity.all
+    render json: activity, status: :ok
+end
+
+def destroy
+    activity =Activity.find_by!(id: [params: :id])
+    activity.destroy
+    render json: {}, status: :no_content
+
+end
+
+private
+
+def not_found_response
+    render json: {error: "Activity not found"}
+
+end
+end      

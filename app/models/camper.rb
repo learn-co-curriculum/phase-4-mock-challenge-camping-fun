27c83f9b3b19c7442bcd8a87e979228fc0f54ccd
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-06 14:23:07 +0300

    created camper resource

---
+++ app/models/camper.rb
@@ -0,0 +1,10 @@
+class Camper < ApplicationRecord
+    has_many :signups
+    has_many :activities, through: :signups
+
+    validates :name, presence: true
+    validates :age, numericality: { greater_than_or_equal_to: 8, less_than_or_equal_to: 18 }
+
+    
+
+end
